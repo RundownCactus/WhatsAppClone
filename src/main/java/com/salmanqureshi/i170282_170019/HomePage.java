f1515c328ecab84558616b9f60936952669322c0
akashali506 <i170019@nu.edu.pk>
2020-11-05 01:08:44 +0500

    Create profile screen added

---
+++ src/main/java/com/salmanqureshi/i170282_170019/HomePage.java
@@ -0,0 +1,14 @@
+package com.salmanqureshi.i170282_170019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
--- src/main/java/com/salmanqureshi/i170282_170019/Register.java
+++ src/main/java/com/salmanqureshi/i170282_170019/Register.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
     TextView signinhere;
+    Button registerbutton1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,14 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Register.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
+        registerbutton1=findViewById(R.id.registerbutton1);
+        registerbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Register.this,CreateProfile.class);
                 startActivity(intent);
             }
         });
